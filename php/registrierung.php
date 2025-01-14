429f1832f74e8ead7ec12d324560f98a5e5d84bf
JoBa02 <jonas.baus@outlook.de>
2025-01-14 12:14:57 +0100

    Schrägstrich

--- php/registrierung.php
+++ php/registrierung.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrierung</title>
     <link rel="stylesheet" href="Sieper.css">
-    <script src="\js\registrierung.js" defer></script>
+    <script src="/js/registrierung.js" defer></script>
 </head>
 <body>
     <header>
